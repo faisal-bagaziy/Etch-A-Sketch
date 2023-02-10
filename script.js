9adbe5daa6f8e5d684ee8cefee6b41b395a5bfec
pablo bagaziy <firstpabloever@gmail.com>
2023-02-10 14:40:21 +0300

    added simple ui

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ let opacityIncrement = 0;
 function color(){
     if(tmp ==0){
         this.style.backgroundColor = 'black';
+        this.style.opacity = 1;
     }else if(tmp == 1){
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
     this.style.backgroundColor = "#" + randomColor;
@@ -29,8 +30,8 @@ function color(){
         this.style.opacity = opacityIncrement ;
     }
 }
-const rockPaperSci = document.querySelectorAll('.grid-item');
-rockPaperSci.forEach(rockPaperSci => rockPaperSci.addEventListener('mouseover',color))
+let grids = document.querySelectorAll('.grid-item');
+grids.forEach(grid => grid.addEventListener('mouseover',color))
 
 let tmp=0;
 function whichOption(){
@@ -48,6 +49,8 @@ options.forEach(option => option.addEventListener('click',whichOption))
 let gridSize = document.querySelector('.prompt');
 gridSize.addEventListener('click', () => {
     let a = prompt("choose gride size max is 100");
-    makeRows(a, 16);
+    makeRows(a,a);
+    grids = document.querySelectorAll('.grid-item');
+    grids.forEach(grid => grid.addEventListener('mouseover',color));
 })
 
